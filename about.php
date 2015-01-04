24797f1061ff989f39cd04375a559df145a9b660
Steve Gardner <gardnersj@gmail.com>
2015-01-04 08:55:23 -0800

    Added links with photos

--- about.php
+++ about.php
@@ -3,32 +3,17 @@ require_once('inc/config.php');
 
 include(ROOT_PATH . 'inc/header.php');
 ?>
-		<div id="main">
-			<img src="img/steve.jpg" alt="Photo of Steve Gardner">
-			<div class="intro">
-				<h1>Tidbits About Steve Gardner</h1>
-				<p>Steve Gardner is a purveyor of delightful experiences. When not refering to himself in the third-person, Steve loves to put smiles on faces. </p>
-				<p>But I much rather prefer the far more expressive first-person. I play music, cook, draw, build, write and code. I try to assault all the senses. And I love to learn. More please!</p>
-				<p>My first fond memories are of dreaming big. Dinosaurs. Spaceships. Flying dogs with capes. After building little motors in my parents and wooden T. Rexes in my parents' basement, college beckoned. I spent my first decade of adulthood bringing happiness to ears. Music was my focus, and my ax was the saxophone. After leaving Cleveland, I wound up in Los Angeles because California. I recorded an album and made a music video.</p>
-				<p>But my interests had grown. The web, and all its inifite creative possibilities, was calling. I read a blog about learning development and was entranced in the siren call of code. I've been learning on Treehouse since the summer. It's fantastic. I'm hooked.</p>
-				<p>In recent news, let's see. There was a wedding to an amazing gorgeous woman. We got a dog. Now we're expecting a little baby boy named August Ellington this srping. He'll be a Gardner too. World, you're welcome.</p>
-				<p>We live in sunny Santa Monica. It's lovely any time of year.</p>
-			</div>
+
+	<div id="main">
+		<img id='portrait' src="img/steve.jpg" alt="Photo of Steve Gardner">
+		<div class="intro">
+			<h1>Tidbits About Steve Gardner</h1>
+			<p>Steve Gardner is a purveyor of delightful experiences. When not refering to himself in the third-person, Steve loves to put smiles on faces. </p>
+			<p>But I much rather prefer the far more expressive first-person. I play music, cook, draw, build, write and code. I try to assault all the senses. And I love to learn. More please!</p>
+			<p>My first fond memories are of dreaming big. Dinosaurs. Spaceships. Flying dogs with capes. After building little motors in my parents and wooden T. Rexes in my parents' basement, college beckoned. I spent my first decade of adulthood bringing happiness to ears. Music was my focus, and my ax was the saxophone. After leaving Cleveland, I wound up in Los Angeles because California. I recorded an album and made a <a id='diggem' href='https://www.youtube.com/watch?v=-nqkyDgt4wA'>music video</a>.</p>
+			<p>But my interests had grown. The web, and all its inifite creative possibilities, was calling. I read a blog <a href='http://rileyh.com/how-i-learned-to-code-in-under-10-months/'>article about learning development</a> and was entranced in the siren call of code. I've been learning on <a id='treehouse' href='http://referrals.trhou.se/stevegardner'>Treehouse</a> since the summer. It's fantastic. I'm hooked.</p>
+			<p>In recent news, let's see. There was a <a id='wedding' href='#'>wedding</a> to an <a id='adina' href='#'>amazing gorgeous woman</a>. We got a <a id='baxter' href='#'>dog</a>. Now we're expecting a little baby boy named August Ellington this srping. He'll be a Gardner too. World, you're welcome.</p>
+			<p>We live in sunny <a href="#">Santa Monica</a>. It's lovely any time of year.</p>
 		</div>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
-    </body>
-</html>
+			
+<?php include(ROOT_PATH . 'inc/footer.php');
